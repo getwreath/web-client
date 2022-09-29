d80aac887a0fbebfcca5f38647ed988284cda462
joshingmachine <josh@joshingmachine.com>
2022-09-28 22:25:41 -0400

    feat: add route for people and pets

---
+++ app/routes/people-and-pets.tsx
@@ -0,0 +1,28 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async () => {
+	return json([
+		{ id: '1', name: 'First Person' },
+		{ id: '2', name: 'Second Person' },
+		{ id: '3', name: 'Third Person' },
+		{ id: '4', name: 'Fourth Person' },
+	]);
+}
+
+export default function BrowsePeopleAndPets() {
+	const peopleAndPets = useLoaderData();
+
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+			<h1>People & Pets</h1>
+
+			<ol>
+				{peopleAndPets.map((personOrPet) => (
+					<li key={personOrPet.id}>{personOrPet.name}</li>
+				))}
+			</ol>
+		</div>
+	);
+}
