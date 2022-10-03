dad9bb42c1e433157ef726c57316dddbe5d4d582
joshingmachine <josh@joshingmachine.com>
2022-10-02 22:17:38 -0400

    feat: add global navigation

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,35 @@
 import type { MetaFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Wreath",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "Wreath",
+	viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<nav>
+					<a href="/">Wreath</a>
+				</nav>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
